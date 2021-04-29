1f1583a66a4a61da1326a0ba55f03da197d2dcf3
Lele Chen <lelechen@fb.com>
2021-04-29 14:51:48 -0400

    g

--- test.py
+++ test.py
@@ -67,7 +67,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
     with torch.no_grad():
         for image_path in os.listdir(dspth):
             img = Image.open(osp.join(dspth, image_path))
-            image = img.resize((512, 512), Image.BILINEAR)
+            # image = img.resize((512, 512), Image.BILINEAR)
             img = to_tensor(image)
             img = torch.unsqueeze(img, 0)
             img = img.cuda()
